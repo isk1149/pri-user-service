9650e0a50c9e9947a82cda6ecd88243d17dd6987
Inseong Kim <isk1149@naver.com>
2024-08-21 20:15:17 +0900

    init

---
+++ src/main/java/com/joongnior/pri_user_service/PriUserServiceApplication.java
@@ -0,0 +1,13 @@
+package com.joongnior.pri_user_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriUserServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joongnior/pri_user_service/PriUserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joongnior.pri_user_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriUserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
